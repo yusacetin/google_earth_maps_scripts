3bd6eddfc35cd5f6173485cebba8268b3f7f0ba8
yusha <yusha493@gmail.com>
2023-11-10 20:51:37 +0300

    update

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Alt + D: Toggle street view
 Alt + S: Exit street view
 
 Alt + F: Reset angle to North-top (only on Google Earth)
+
+### Known Issues
+Alt + F doesn't work on the Linux X11 script for some reason. Also, since Alt + D is already the default Google Chrome shortcut to focus the URL bar there might be a slight time window where the URL bar flashes every time this shortcut is activated. You can easily change the keys from the script files but I'm used to these keys from my old extension so I plan to keep these defaults for now.
--- google_earth_linux_x11.py
+++ google_earth_linux_x11.py
@@ -1,9 +1,10 @@
 ################################################################################################
-# Google Maps custom keyboard shortcuts
+# Google Earth custom keyboard shortcuts
 # Works on Linux with X11
 # Default margin values for 1920 by 1080 resolution and whatever my screen's pixel density is
 # Tested on Debian 12 with GNOME 43
-# Dependencies: pynput, xdotool, xprop, grep
+# Dependencies: pynput, xdotool, xprop
+# North-Top doesn't work for some reason
 ################################################################################################
 
 from pynput.keyboard import Key, Listener
@@ -12,17 +13,22 @@ import subprocess
 
 STREET_VIEW_ENTER_KEY = "d"
 STREET_VIEW_EXIT_KEY = "s"
+NORTH_TOP_KEY = "f"
 
-STREET_VIEW_ENTER_XMARGIN_MAXIMIZED = -150 # from window right edge
-STREET_VIEW_ENTER_YMARGIN_MAXIMIZED = -34 # from window bottom edge
-STREET_VIEW_EXIT_XMARGIN_MAXIMIZED = -30 # from window right edge
-STREET_VIEW_EXIT_YMARGIN_MAXIMIZED = 110 # from window top edge
+STREET_VIEW_ENTER_XMARGIN_MAXIMIZED = -35 # from window right edge
+STREET_VIEW_ENTER_YMARGIN_MAXIMIZED = -130 # from window bottom edge
+STREET_VIEW_EXIT_XMARGIN_MAXIMIZED = 30 # from window left edge
+STREET_VIEW_EXIT_YMARGIN_MAXIMIZED = 120 # from window top edge
+NORTH_TOP_XMARGIN_MAXIMIZED = -35 # from window right edge
+NORTH_TOP_YMARGIN_MAXIMIZED = -180 # from window bottom edge
 
 # Margins are slightly different when the window is not maximized
 STREET_VIEW_ENTER_XMARGIN_NOT_MAXIMIZED = STREET_VIEW_ENTER_XMARGIN_MAXIMIZED - 15 # from window right edge
 STREET_VIEW_ENTER_YMARGIN_NOT_MAXIMIZED = STREET_VIEW_ENTER_YMARGIN_MAXIMIZED - 35 # from window bottom edge
-STREET_VIEW_EXIT_XMARGIN_NOT_MAXIMIZED = STREET_VIEW_EXIT_XMARGIN_MAXIMIZED - 22 # from window right edge
-STREET_VIEW_EXIT_YMARGIN_NOT_MAXIMIZED = STREET_VIEW_EXIT_YMARGIN_MAXIMIZED + 15 # from window top edge
+STREET_VIEW_EXIT_XMARGIN_NOT_MAXIMIZED = STREET_VIEW_EXIT_XMARGIN_MAXIMIZED + 20 # from window left edge
+STREET_VIEW_EXIT_YMARGIN_NOT_MAXIMIZED = STREET_VIEW_EXIT_YMARGIN_MAXIMIZED + 20 # from window top edge
+NORTH_TOP_XMARGIN_NOT_MAXIMIZED = NORTH_TOP_XMARGIN_MAXIMIZED - 15 # from window right edge
+NORTH_TOP_YMARGIN_NOT_MAXIMIZED = NORTH_TOP_YMARGIN_MAXIMIZED - 30 # from window bottom edge
 
 mouse = Controller()
 alt_active = False # whether the Alt key is currently being pressed or not, used to check for key combinations with Alt
@@ -57,7 +63,7 @@ def on_press(key):
     global alt_active
     try: # key.char throws an exception when non-alphanumeric keys are pressed
         window_title = subprocess.getoutput("xdotool getactivewindow getwindowname")
-        if "Google Maps" in window_title:
+        if "Google Earth" in window_title:
             if key == Key.alt:
                 alt_active = True # update Alt key pressed status
 
@@ -81,15 +87,30 @@ def on_press(key):
             elif key.char == STREET_VIEW_EXIT_KEY: # exit street view (clicks the profile button if used in map view)
                 if alt_active:
                     (window_x, window_y, window_w, window_h) = get_active_window_geometry()
-                    click_x = window_x + window_w + STREET_VIEW_EXIT_XMARGIN_MAXIMIZED
+                    click_x = window_x + STREET_VIEW_EXIT_XMARGIN_MAXIMIZED
                     click_y = window_y + STREET_VIEW_EXIT_YMARGIN_MAXIMIZED
 
                     window_id = subprocess.getoutput("xdotool getactivewindow")
                     window_status_cmd = "xprop -id " + window_id + " _NET_WM_STATE"
                     window_status = subprocess.getoutput(window_status_cmd)
                     if not "_NET_WM_STATE_MAXIMIZED_VERT" in window_status and not "_NET_WM_STATE_MAXIMIZED_HORZ" in window_status:
-                        click_x = window_x + window_w + STREET_VIEW_EXIT_XMARGIN_NOT_MAXIMIZED
+                        click_x = window_x + STREET_VIEW_EXIT_XMARGIN_NOT_MAXIMIZED
                         click_y = window_y + STREET_VIEW_EXIT_YMARGIN_NOT_MAXIMIZED
+
+                    click_and_return(click_x, click_y)
+
+            elif key.char == NORTH_TOP_KEY: # set view to north to top
+                if alt_active:
+                    (window_x, window_y, window_w, window_h) = get_active_window_geometry()
+                    click_x = window_x + window_w + NORTH_TOP_XMARGIN_MAXIMIZED
+                    click_y = window_y + window_h + NORTH_TOP_YMARGIN_MAXIMIZED
+
+                    window_id = subprocess.getoutput("xdotool getactivewindow")
+                    window_status_cmd = "xprop -id " + window_id + " _NET_WM_STATE"
+                    window_status = subprocess.getoutput(window_status_cmd)
+                    if not "_NET_WM_STATE_MAXIMIZED_VERT" in window_status and not "_NET_WM_STATE_MAXIMIZED_HORZ" in window_status:
+                        click_x = window_x + window_w + NORTH_TOP_XMARGIN_NOT_MAXIMIZED
+                        click_y = window_y + window_h + NORTH_TOP_YMARGIN_NOT_MAXIMIZED
 
                     click_and_return(click_x, click_y)
     except:
